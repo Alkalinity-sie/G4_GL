e1d6daaeeadd8d1456ce2a4424a491359e55057b
muratokutucu <54741414+muratokutucu@users.noreply.github.com>
2020-02-27 20:29:53 +0100

    Add files via upload

---
+++ couchedepersistance/LocationDao.java
@@ -0,0 +1,44 @@
+package couchedepersistance;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.ImageIcon;
+
+public interface LocationDao {
+    //retrieval of the list of all existing locations (all maps included)
+    default List<Location> getLocations (){
+        List<Location> locations = new ArrayList<>();
+        for(int i = 0; i < 30; i++){
+            locations.add(new Location());
+        }
+        return locations;
+    }
+    //retrieval of a location's name
+    default String getName (int location_id){
+        return "random name";
+    }
+    //retrieval of a location's description
+    default String getDescription (int location_id){
+        return "random description";
+    }
+    //retrieval de a location's address
+    default String getAddress (int location_id){
+        return "random address";
+    }
+    //retrieval of a location's list of labels
+    default List<String> getLabels (int location_id){
+        List<String> labels = new ArrayList<>();
+        for(int i = 0; i < 5; i++){
+            labels.add("label "+i);
+        }
+        return labels;
+    }
+    //retrieval of a location's list of photos
+    default List<ImageIcon> getPhotos (int location_id){
+        List<ImageIcon> photos = new ArrayList<>();
+        for(int i = 0; i < 10; i++){
+            photos.add(new ImageIcon());
+        }
+        return photos;
+    }
+}
---
+++ couchedepersistance/User.java
@@ -0,0 +1,63 @@
+package couchedepersistance;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private int id;
+    private static int numberOfExistingUser = 0;
+    private String username;
+    private String password;
+    private List<Map> sharedToMe;
+    private List<Map> myMaps;
+    
+    public User (){
+        numberOfExistingUser++;
+        this.id = numberOfExistingUser;
+        this.username = "";
+        this.password = "";
+        this.sharedToMe = new ArrayList<>();
+        this.myMaps = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Map> getSharedToMe() {
+        return sharedToMe;
+    }
+
+    public void setSharedToMe(List<Map> sharedToMe) {
+        this.sharedToMe = sharedToMe;
+    }
+
+    public List<Map> getMyMaps() {
+        return myMaps;
+    }
+
+    public void setMyMaps(List<Map> myMaps) {
+        this.myMaps = myMaps;
+    }
+    
+}
