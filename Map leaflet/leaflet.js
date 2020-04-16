c1bc15a7d18268208d3166eef621a1961b906105
TMFayas <61230045+TMFayas@users.noreply.github.com>
2020-04-16 14:06:56 +0200

    Add files via upload

--- Map leaflet/leaflet.js
+++ Map leaflet/leaflet.js
@@ -1,6 +1,14 @@
+
+var mymap;
+function onMapClick(e) {
+    		//alert("You clicked the map at " + e.latlng);
+    		var marker = L.marker(e.latlng).addTo(mymap);
+    		//return marker;
+}
+
 $(function (){
 
-		var mymap = L.map('mapleflet1').setView([51.505, -0.09], 13);
+		mymap = L.map('mapleflet1').setView([48.866667, 2.333333], 11);
 		L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     	maxZoom: 18,
@@ -9,5 +17,9 @@ $(function (){
     	zoomOffset: -1,
     	accessToken: 'pk.eyJ1IjoibGVkb2RvaW5jb25udSIsImEiOiJjazg3OXNkZWUwZDdiM2VwcmQxbjI0cWczIn0.gsqi-Jp2FZ066BoQ_0IEJQ'
 		}).addTo(mymap);
+
+		mymap.on('click', onMapClick);
+		L.control.scale().addTo(mymap);
+		var searchControl = L.esri.Geocoding.geosearch().addTo(mymap);
 
 });
